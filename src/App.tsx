4088c75cfc4e5b62ee5c61bf4dba05cf216b54fc
Jairo Rodriguez <jairodri.a05@gmail.com>
2025-11-12 18:46:06 +0000

    subir cambios

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import { Grid } from '@mui/material'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <Grid>
+    <Grid container spacing={5} justifyContent="center" alignItems="center">
 
       {/* Encabezado */}
-      <Grid>Elemento: Encabezado</Grid>
+      <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
 
       {/* Alertas */}
-      <Grid>Elemento: Alertas</Grid>
+      <Grid >Elemento: Alertas</Grid>
 
       {/* Selector */}
-      <Grid>Elemento: Selector</Grid>
+      <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector</Grid>
 
       {/* Indicadores */}
-      <Grid>Elemento: Indicadores</Grid>
+      <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
 
       {/* Gráfico */}
-      <Grid>Elemento: Gráfico</Grid>
+      <Grid sx={{ display: { xs: "none", md: "block"} }}>Elemento: Gráfico</Grid>
 
       {/* Tabla */}
-      <Grid>Elemento: Tabla</Grid>
+      <Grid sx={{ display: { xs: "none", md: "block" } }}>Elemento: Tabla</Grid>
 
       {/* Información adicional */}
       <Grid>Elemento: Información adicional</Grid>
