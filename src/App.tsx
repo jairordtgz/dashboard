a9f579bc7b0b797faa1a4152fe117bbfccd73b8d
Jairo Rodriguez <jairodri.a05@gmail.com>
2025-12-01 19:48:18 +0000

    avance guia 14 01-12-25

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Grid } from '@mui/material'
 import HeaderUI from './components/HeaderUI'
 import AlertUI from './components/AlertUI'
+import SelectorUI from './components/SelectorUI'
 
 function App() {
 
@@ -20,7 +21,10 @@ function App() {
       </Grid>
 
       {/* Selector */}
-      <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector</Grid>
+      <Grid size={{ xs: 12, md: 3  }}>
+        <SelectorUI/>
+
+      </Grid>
 
       {/* Indicadores */}
       <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
